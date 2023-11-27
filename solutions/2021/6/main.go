589cae74a84afb85ff000d41c7a5cb57b97a90b2
Haugen <tobiashaugen@gmail.com>
2023-11-27 16:04:01 +0100

    2021 day 6 part 1

---
+++ solutions/2021/6/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/Haugen/aoc-go/internal/utils"
+)
+
+var data []string
+
+func main() {
+	path, _ := os.Getwd()
+	data = utils.FilenameToArray(path + "/input.txt")
+
+	part1()
+}
+
+func part1() {
+	dataArrStr := strings.Split(data[0], ",")
+	dataArr := utils.StrArrayToIntArray(dataArrStr)
+
+	var count int
+	for i := 0; i < 256; i++ {
+		count = 0
+		for j := 0; j < len(dataArr); j++ {
+			if dataArr[j] == 0 {
+				count++
+				dataArr[j] = 6
+			} else {
+				dataArr[j]--
+			}
+		}
+
+		for x := 0; x < count; x++ {
+			dataArr = append(dataArr, 8)
+		}
+	}
+
+	fmt.Println(len(dataArr))
+}
